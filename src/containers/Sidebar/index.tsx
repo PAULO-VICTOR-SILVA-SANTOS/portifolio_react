dee1f7cd855b3c8f6d59c7a4a707d4e45ce2e429
PAULO-VICTOR-SILVA-SANTOS <paulinhovictor852@gmail.com>
2026-03-05 17:38:57 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://avatars.githubusercontent.com/u/169206299?v=4" />
+    <Titulo fontSize={20}>Paulo Victor</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, possimus
+      odio repudiandae magni quos earum accusantium ipsum aut, sed quisquam
+      voluptatum recusandae voluptate consectetur porro fugit provident cum
+      laboriosam hic.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=PAULO-VICTOR-SILVA-SANTOS&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=PAULO-VICTOR-SILVA-SANTOS&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
